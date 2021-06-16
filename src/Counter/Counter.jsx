f44fe817b84078d9688fae92450cc7638b7c2b24
Harshvardhan <67060748+HarshVardhan22@users.noreply.github.com>
2021-06-16 21:08:17 +0530

    first commit

---
+++ src/Counter/Counter.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "react-bootstrap";
+function Counter() {
+  return (
+    <div>
+      <h1>Counter</h1>
+      <Button className="btn-primary">+1</Button>
+    </div>
+  );
+}
+
+export default Counter;
