3a9e302a6c646efdabd4a0831bb264b40c28ab8b
Harshvardhan <67060748+HarshVardhan22@users.noreply.github.com>
2021-06-18 18:29:35 +0530

    edit feature in progress

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,24 +1,44 @@
-import React, { useState } from "react";
+import React, { useState, useEffect} from "react";
 import { Button } from "react-bootstrap";
+import firebaseDb from "../firebase";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEdit, faPen, faTrash } from '@fortawesome/free-solid-svg-icons'
 
-const Form = (props) => {
+const Form = () => {
+
   const [task, setTask] = useState("");
-  const [taskArr, setTaskArr] = useState([]);
-
+  const [id,setId] = useState();
   const handleTask = (e) => {
     setTask([e.target.value]);
   };
 
-//   const handleTaskArr = () => {
-//     setTaskArr([...taskArr, task]);
-//     setTask("");
-//   };
-
   const addTask = (e) => {
     e.preventDefault();
-    props.addOrEdit(task);
+    addOrEdit(task);
     setTask("");
   };
+
+  const addOrEdit = (obj) => {
+    firebaseDb.child("task").push(obj, (err) => {
+      if (err) console.log(err);
+    });
+  };
+
+  const update = (id,e) =>{
+    console.log(id);
+    console.log(e.target.value);
+    e.target.value="";
+  }
+
+  const [taskObject, setTaskObject] = useState({});
+  useEffect(() => {
+    firebaseDb.child("task").on("value", (snapshot) => {
+      if (snapshot.val() != null)
+        setTaskObject({
+          ...snapshot.val(),
+        });
+    });
+  }, []);
 
   return (
     <>
@@ -30,11 +50,7 @@ const Form = (props) => {
             name="task"
             value={task}
             onChange={handleTask}
-          />
-          {taskArr}
-          {/* <Button className="btn-danger mt-2" onClick={handleTaskArr}>
-            Add
-          </Button> */}
+          />       
         </div>
         <div className="form-group">
           <button
@@ -43,7 +59,31 @@ const Form = (props) => {
             style={{width:"100px",marginLeft:"40%"}}
           >Add</button>
         </div>
+       
+        <div>
+        {Object.keys(taskObject).map((id) => {
+        return (         
+            <div className="row">            
+              <div className="col-6 offset-3">
+              <input
+                className="form-control mt-2 text-dark"
+                type="text"
+                style={{ backgroundColor: "#FF0000" }}
+                value={taskObject[id][0]}
+                onChange={(e)=>{update(id,e)}}
+              />
+              </div>
+              {/* <FontAwesomeIcon icon={faPen} style={{marginTop:"3%",marginRight:"3%"}}/> */}
+              <FontAwesomeIcon icon={faTrash} style={{marginTop:"3%"}}/>
+            </div>                   
+        );
+      })} 
+        
+        </div>
       </form>
+      
+      
+
     </>
   );
 };
